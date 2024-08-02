2f8dec80684d39c0a30f7670e59b38dcd805114b
Xav86 <gustavo.g.2005@gmail.com>
2024-08-02 16:54:31 -0300

    trabalhando com v-for

--- lista-interativa/src/App.vue
+++ lista-interativa/src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="container">
     <h1>Titulo de teste</h1>
-    <input type="text" v-model="dados.nome">
-    <ClientePage :dados="dados" :showIdade="true"/>
-    <ClientePage :dados="dados" :showIdade="false"/>
-    <ClientePage :dados="dados" :showIdade="false"/>
-    <ClientePage :dados="dados" :showIdade="true"/>
+    <div v-for="(cliente, index) in clientes" :key="cliente.id">
+      <h1>{{ index + 1 }}</h1>
+      <ClientePage :dados="cliente"/>
+      <h4>Edição</h4>
+      <input type="text" v-model="cliente.nome">
+      <input type="text" v-model="cliente.email">
+    </div>
   </div>
 </template>
 
@@ -24,7 +26,33 @@ export default {
         nome: 'Gustavo',
         email: 'gustavo@gustavo.com',
         idade: 22
-      }
+      },
+      clientes: [
+        {
+          id: 2,
+          nome: 'Gustavo',
+          email: 'gustavo@gustavo.com',
+          idade: 22
+        },
+        {
+          id: 22,
+          nome: 'Caetano',
+          email: 'caetano@caetano.com',
+          idade: 224
+        },
+        {
+          id: 222,
+          nome: 'Calebe',
+          email: 'calebe@calebe.com',
+          idade: 222
+        },
+        {
+          id: 222,
+          nome: 'Calege',
+          email: 'calege@calege.com',
+          idade: 2222
+        }
+      ]
     }
   }
 }
