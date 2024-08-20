1d6588bed5cfa64d0a6b4eed1ec53405e5f2c557
Xav86 <gustavo.g.2005@gmail.com>
2024-08-20 11:37:57 -0300

    fazendo a base do projeto

---
+++ gerenciador de usuários/vue-users/src/components/FormComponent.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="box">
+        <label for="email" class="lable">Email:</label>
+        <input class="form-control" type="email" id="email" name="email" placeholder="email@email.com">
+    </div>
+</template>
+
+<style>
+    .box {
+        display: flex;
+        flex-direction: column;
+        align-items: left;
+        background-color: #e3e3e3;
+        border-radius: 10px;
+        padding: 10px 20px;
+    }
+
+    .lable {
+        font-weight: 600;
+    }
+</style>
---
+++ gerenciador de usuários/vue-users/src/views/RegisterView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+    <h1>Cadastro</h1>
+    <FormComponent />
+  </div>
+</template>
+
+<script>
+import FormComponent from '../components/FormComponent.vue';
+
+export default {
+    components: {
+        FormComponent
+    }
+}
+</script>
+
+<style>
+    h1 {
+        text-align: center;
+    }
+
+    .container {
+        display: flex;
+        flex-direction: column;
+        gap: 7px;
+    }
+</style>
